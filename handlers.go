616d4da8a5cdb536149902ed24c57a223b34a927
iampeterbanjo <peterbanjo@gmail.com>
2016-09-19 06:25:27 +0100

    failing on server.go newInMemoryRepository

--- handlers.go
+++ handlers.go
@@ -3,13 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/cloudnativego/gogo-engine"
 	"github.com/google/uuid"
 	"github.com/unrolled/render"
 )
 
-func createMatchHandler(formatter *render.Render) http.HandlerFunc {
+func createMatchHandler(formatter *render.Render, repo matchRepository) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Location", "some value")
+		newMatch := gogo.NewMatch(5)
+		repo.addMatch(newMatch)
 		guid := uuid.New().String()
 		w.Header().Add("Location", "/matches/"+guid)
 		formatter.JSON(w, http.StatusCreated, &newMatchResponse{ID: guid})
